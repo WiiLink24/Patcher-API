1eb6d3cf6dfc6dfd60123647f3d504e9163519c1
Alex <basurillodesgastorito@gmail.com>
2025-04-20 11:09:57 +0200

    Chroe: Removed unnecesary OIDC information

--- utils.py
+++ utils.py
@@ -4,13 +4,9 @@ def get_user_info():
     # Get all relevant user information at once
     user_info = oidc.user_getinfo([
         'email', 
-        'sub', 
         'preferred_username', 
         'picture', 
         'name',
-        'groups',
-        'given_name',
-        'family_name'
     ])
     
     profile_picture = user_info.get('picture')
@@ -23,10 +19,5 @@ def get_user_info():
                      user_info.get('name',
                        user_info.get('email', 'User'))),
         'profile_picture': profile_picture,
-        'email': user_info.get('email'),
-        'full_name': user_info.get('name', ''),
-        'first_name': user_info.get('given_name', ''),
-        'last_name': user_info.get('family_name', ''),
-        'groups': user_info.get('groups', []),
-        'is_admin': 'admin' in user_info.get('groups', [])
+        'full_name': user_info.get('name', '')
     }
